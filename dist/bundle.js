3f636e24483a239ec2ce3aa7966d351d72fe42bb
Adam Gray <adam@adamgray.so>
2017-02-08 16:48:41 +0000

    Update NoMatch

--- dist/bundle.js
+++ dist/bundle.js
@@ -31530,6 +31530,8 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
+	var _reactRouter = __webpack_require__(178);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -31552,8 +31554,23 @@
 	    value: function render() {
 	      return _react2.default.createElement(
 	        'div',
-	        null,
-	        'Page not found'
+	        { className: 'container text-center' },
+	        _react2.default.createElement(
+	          'h1',
+	          null,
+	          '404'
+	        ),
+	        _react2.default.createElement(
+	          'h2',
+	          null,
+	          'Page not found'
+	        ),
+	        _react2.default.createElement('hr', null),
+	        _react2.default.createElement(
+	          _reactRouter.IndexLink,
+	          { to: '/' },
+	          'Back to app'
+	        )
 	      );
 	    }
 	  }]);
--- src/app/components/NoMatch/index.js
+++ src/app/components/NoMatch/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {IndexLink} from 'react-router'
 
 class NoMatch extends React.Component {
   render() {
     return (
-      <div>
-        Page not found
+      <div className="container text-center">
+        <h1>404</h1>
+        <h2>Page not found</h2>
+        <hr/>
+        <IndexLink to="/">Back to app</IndexLink>
       </div>
     )
   }
